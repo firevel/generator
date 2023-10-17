d7a63428830604ecbc0a53b48e44ceffbdbae553
Michael Slowik <slowik.michael@gmail.com>
2023-10-17 12:14:24 -0500

    migration generator

--- src/Generators/ApiResource/MigrationGenerator.php
+++ src/Generators/ApiResource/MigrationGenerator.php
@@ -10,18 +10,61 @@ class MigrationGenerator extends BaseGenerator
     public function handle()
     {
         $resource = $this->resource();
+        $name = date('Y_m_d_His') . "_create_{$resource->name()->plural()->snake()}_table";
 
-        $name = "create_{$resource->name()->plural()->snake()}_table";
-
-        $this->artisan(
-            'make:migration',
+        $source = $this->render(
+            'api-resource/migration',
             [
-                'name' => $name,
-                '--create' => $resource->name()->plural()->snake(),
+                'resource' => $resource,
             ]
         );
+
+
+        $path = database_path('migrations' . '/' . "{$name}.php");
+
+        $this->createFile($path, $source);
+
         $this->logger()->info("# Migration created: {$name}");
         $this->logger()->info('- [Required] Set migration');
         $this->logger()->info('  - Available column types https://laravel.com/docs/migrations#available-column-types)');
     }
+
+    function generateMigrationCode($chains) {
+        $lines = [];
+
+        foreach ($chains as $chainMethods) {
+            $line = "\$table";
+
+            foreach ($chainMethods as $method) {
+                $methodName = $method['name'];
+
+                $params = [];
+                if (isset($method['params'])) {
+                    foreach ($method['params'] as $param) {
+                        if (is_string($param)) {
+                            $params[] = "'$param'";
+                        } elseif (is_array($param)) {
+                            $arrayValues = array_map(function ($value) {
+                                return "'$value'";
+                            }, $param);
+                            $params[] = "[" . implode(', ', $arrayValues) . "]";
+                        } else {
+                            $params[] = $param;
+                        }
+                    }
+                }
+
+                $paramString = implode(', ', $params);
+                $line .= "->{$methodName}({$paramString})";
+            }
+
+            $line .= ";";
+            $lines[] = $line;
+        }
+
+        return $lines;
+    }
+
+
+
 }
