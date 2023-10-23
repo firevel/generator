3055d45d85b674ed51fa4b44e006390a9e4f20df
Michael Slowik <slowik.michael@gmail.com>
2023-10-23 11:32:05 -0500

    add nl between migration lines

--- stubs/api-resource/migration.blade.php
+++ stubs/api-resource/migration.blade.php
@@ -25,6 +25,7 @@ return new class extends Migration
                     return $param;
                 })
                 ->implode(', ')!!}){{$loop->last ? ';': ''}}@endforeach
+
 @endforeach
         });
 @endif
