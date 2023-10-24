a731c3192c46e3c2d19cc00cb8f6619c984d3e79
Michael Slowik <slowik.michael@gmail.com>
2023-10-23 21:58:22 -0500

    fix factory template syntax

--- stubs/api-resource/factory.blade.php
+++ stubs/api-resource/factory.blade.php
@@ -8,13 +8,13 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\{{$resource->name()->singular()->studly()}}>
+ * {{ '@' }}extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\{{$resource->name()->singular()->studly()}}>
  */
 class {{$resource->name()->singular()->studly()}}Factory extends Factory
 {
 @if ($resource->has('factories.definition'))
     /**
-     * Define the models default state.
+     * Define the model's default state.
      *
      * @return array<string, mixed>
      */
@@ -22,15 +22,14 @@ class {{$resource->name()->singular()->studly()}}Factory extends Factory
     {
         return [
 @foreach ($resource->factories['definition'] as $attribute => $chain)
-            '{{$attribute}}' => }@foreach ($chain as $method => $params){{$loop->first ? '': '->'}}{{$method}}({!! empty($params) ? '' : collect($params)
+            '{{$attribute}}' => @foreach ($chain as $method => $params){!!$loop->first ? '': '->'!!}{{$method}}({!! empty($params) ? '' : collect($params)
                 ->transform(function($param) {
                     if (is_string($param) && $param[0] !== '$') {
                         return "'{$param}'";
                     }
                     return $param;
                 })
-                ->implode(', ')!!}),@endforeach
-
+                ->implode(', ')!!})@endforeach,
 @endforeach
         ];
     }
